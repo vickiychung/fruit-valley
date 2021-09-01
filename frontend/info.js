6f06ca1cc0a320559d133f98a3e06f329ff3d589
vickiychung <vickiychung@gmail.com>
2021-08-31 21:20:05 -0700

    Fetch specific fruit info

--- frontend/info.js
+++ frontend/info.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
 
-axios.get('https://www.fruityvice.com/api/fruit/all')
-  .then(function (response) {
-    console.log(response.data);
-  })
+const fetchFruit = (fruitName) => {
+  axios.get(`https://www.fruityvice.com/api/fruit/${fruitName}`)
+    .then(response => {
+      console.log(response.data) ;
+    })
+}
+
+// axios.get('https://www.fruityvice.com/api/fruit/all')
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
