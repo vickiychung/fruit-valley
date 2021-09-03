516b1b30e873027576ab3890f7e2420c723b845e
vickiychung <vickiychung@gmail.com>
2021-09-02 18:55:14 -0700

    Exclude fruits with no images

--- frontend/src/components/fruits/fruits_index_item.jsx
+++ frontend/src/components/fruits/fruits_index_item.jsx
@@ -7,16 +7,20 @@ class FruitsIndexItem extends React.Component {
 
   render() {
     const { fruit, imgUrl } = this.props;
-    
-    return(
-      <li className="indiv-fruit">
-        {fruit.name}
 
-        <img className="fruit-pic"
-          src={imgUrl.toLowerCase()} 
-          alt={fruit.name} />
-      </li>
-    ) 
+    return (fruit.name !== 'Grapes') && 
+      (fruit.name !== 'Lime') && 
+      (fruit.name !== 'Melon') && 
+      (fruit.name !== 'Persimmon') && 
+      (fruit.name !== 'Papaya') 
+      ? <li className="indiv-fruit">
+          {fruit.name}
+
+          <img className="fruit-pic"
+            src={imgUrl.toLowerCase()} 
+            alt={fruit.name} />
+        </li>
+      : '';
   }
 }
 
