bccbafa995635d939c84b9dbfbe3d151b89204b8
vickiychung <vickiychung@gmail.com>
2021-09-03 14:57:23 -0700

    Create modal reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import fruitsReducer from './fruits_reducer';
+import modalReducer from './modal_reducer';
 
 const rootReducer = combineReducers({
-  fruits: fruitsReducer
+  fruits: fruitsReducer,
+  modal: modalReducer
 });
 
 export default rootReducer;
