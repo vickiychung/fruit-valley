2090195c6e594fb34c224699f6fc894fbe857f4b
vickiychung <vickiychung@gmail.com>
2021-09-03 16:39:53 -0700

    Fetch all fruit information

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -6,7 +6,8 @@ const Modal = function ({ fruit, closeModal }) {
     return null;
   }
 
-  const imgUrl = `https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/${fruit.name}.png`
+  const imgUrl = `https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/${fruit.name}.png`;
+  const nutritions = fruit.nutritions;
  
   return (
 
@@ -21,7 +22,20 @@ const Modal = function ({ fruit, closeModal }) {
         </div>
 
         <div className="modal-right">
+          <p>Genus: {fruit.genus}</p>
+          <p>Family: {fruit.family}</p>
+          <p>Order: {fruit.order}</p>
 
+          <span>
+            Nutritions:
+            <ul>
+              <li>Carbohydrates: {nutritions.carbohydrates}</li>
+              <li>Protein: {nutritions.protein}</li>
+              <li>Fat: {nutritions.fat}</li>
+              <li>Calories: {nutritions.calories}</li>
+              <li>Sugar: {nutritions.sugar}</li>
+            </ul>
+          </span>
         </div>
       </div>
     </div>
