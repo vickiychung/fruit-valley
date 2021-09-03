c84491501de1b33f380fb089c68a5a1eb042df37
vickiychung <vickiychung@gmail.com>
2021-09-03 15:07:31 -0700

    Set up modal container

--- frontend/src/components/modal/modal_container.js
+++ frontend/src/components/modal/modal_container.js
@@ -1,1 +1,16 @@
 import { connect } from 'react-redux';
+import { closeModal } from '../../actions/modal_actions';
+
+const mapStateToProps = state => {
+  return {
+    modal: state.modal
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)();
