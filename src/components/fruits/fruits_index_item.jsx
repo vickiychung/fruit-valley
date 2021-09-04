ba89b335c8c81b837d3c4bb58992b006f2426020
vickiychung <vickiychung@gmail.com>
2021-09-04 16:35:08 -0700

    Remove constructor

--- src/components/fruits/fruits_index_item.jsx
+++ src/components/fruits/fruits_index_item.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class FruitsIndexItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { fruit, imgUrl, openModal } = this.props;
 
