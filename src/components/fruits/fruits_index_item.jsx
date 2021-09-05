9a84295401f02ba2eeb6345d2dd055c01bad65a8
vickiychung <vickiychung@gmail.com>
2021-09-04 18:40:17 -0700

    Limit fruit items

--- src/components/fruits/fruits_index_item.jsx
+++ src/components/fruits/fruits_index_item.jsx
@@ -4,11 +4,21 @@ class FruitsIndexItem extends React.Component {
   render() {
     const { fruit, imgUrl, openModal } = this.props;
 
-    return (fruit.name !== 'Grapes') && 
-      (fruit.name !== 'Lime') && 
-      (fruit.name !== 'Melon') && 
-      (fruit.name !== 'Persimmon') && 
-      (fruit.name !== 'Papaya') 
+    return (fruit.name === 'Apple') ||
+        (fruit.name === 'Apricot') ||
+        (fruit.name === 'Banana') ||
+        (fruit.name === 'Blueberry') ||
+        (fruit.name === 'Cherry') ||
+        (fruit.name === 'Guava') ||
+        (fruit.name === 'Lemon') ||
+        (fruit.name === 'Mango') ||
+        (fruit.name === 'Orange') ||
+        (fruit.name === 'Pear') ||
+        (fruit.name === 'Pineapple') ||
+        (fruit.name === 'Raspberry') ||
+        (fruit.name === 'Strawberry') ||
+        (fruit.name === 'Tomato') ||
+        (fruit.name === 'Watermelon')
       ? <li className="indiv-fruit" onClick={() => openModal(fruit)}>
           <img className="fruit-pic"
             src={imgUrl.toLowerCase()} 
