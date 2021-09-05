03c81415785e57bedca3d63dfa1eb263d0d5482e
vickiychung <vickiychung@gmail.com>
2021-09-04 18:07:03 -0700

    Troubleshoot deployment

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
 import FruitsIndexContainer from './fruits/fruits_index_container';
 import ModalContainer from './modal/modal_container';
 
 const App = () => (
   <div>
     <ModalContainer />
-    <FruitsIndexContainer />
+
+    <Switch>
+      <Route exact path={process.env.PUBLIC_URL + '/'} component={FruitsIndexContainer} />
+      <Redirect exact from={process.env.PUBLIC_URL + '/*'} to={process.env.PUBLIC_URL + '/'} />
+    </Switch>
   </div>
 );
 
