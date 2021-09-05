a0404f9b33c16fcc42dbe4976ef214148db1c807
vickiychung <vickiychung@gmail.com>
2021-09-04 18:27:56 -0700

    Use browser router

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Redirect, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import FruitsIndexContainer from './fruits/fruits_index_container';
 import ModalContainer from './modal/modal_container';
 
@@ -10,10 +10,6 @@ const App = () => (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <FruitsIndexContainer />
     </BrowserRouter>
-    {/* <Switch>
-      <Route exact path={process.env.PUBLIC_URL + '/'} component={FruitsIndexContainer} />
-      <Redirect exact from={process.env.PUBLIC_URL + '/*'} to={process.env.PUBLIC_URL + '/'} />
-    </Switch> */}
   </div>
 );
 
