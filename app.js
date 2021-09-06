c6bdb8cca875137aa4fe9288e99663443892546e
vickiychung <vickiychung@gmail.com>
2021-09-06 15:10:23 -0700

    Test heroku push

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  })
+}
 
 app.get('/api/fruit/all', (req, res) => {
   fetch('https://www.fruityvice.com/api/fruit/all')
