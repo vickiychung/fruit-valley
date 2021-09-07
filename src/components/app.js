3aa4015c5febd18168cb7c31231189cdc95c988b
vickiychung <vickiychung@gmail.com>
2021-09-06 18:11:29 -0700

    Remove cors backend

--- src/components/app.js
+++ src/components/app.js
@@ -1,63 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import FruitsIndexContainer from './fruits/fruits_index_container';
 import ModalContainer from './modal/modal_container';
-// import { useEffect, useState } from 'react';
 
-export default class App extends Component {
-  state = {
-    message: "",
-    error: "",
-    eee: "",
-    text: ""
-  };
+const App = () => (
+  <div>
+    <ModalContainer />
 
-  componentDidMount = () => this.fetchAPIMessage();
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <FruitsIndexContainer />
+    </BrowserRouter>
+  </div>
+);
 
-  fetchAPIMessage = async () => {
-    try {
-      const res = await fetch(`/api/fruit/all`);
-      const { message } = await res.json();
-      this.setState({ message });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  render = () => (
-    <div>
-      <ModalContainer />
-
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <FruitsIndexContainer />
-      </BrowserRouter>
-    </div>
-  );
-}
-
-// function App() {
-//   const makeAPICall = async () => {
-//     try {
-//       const response = await fetch('https://www.fruityvice.com/api/fruit/all', { mode: 'cors' });
-//       const data = await response.json();
-//       console.log({ data })
-//     }
-//     catch (e) {
-//       console.log(e)
-//     }
-//   }
-//   useEffect(() => {
-//     makeAPICall();
-//   }, [])
-//   return (
-    // <div>
-    //   <ModalContainer />
-
-    //   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    //     <FruitsIndexContainer />
-    //   </BrowserRouter>
-    // </div>
-//   );
-// }
-
-// export default App;
+export default App;
